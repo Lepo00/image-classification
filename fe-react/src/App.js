3e45be2d6ccdcc415bc67fcbbaae345819890b47
leporati_luca <l.leporati@anoki.it>
2023-08-06 19:22:08 +0200

    add state & upload input

--- fe-react/src/App.js
+++ fe-react/src/App.js
@@ -1,7 +1,47 @@
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [model, setModel] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const loadModel = async () => {
+    setIsModelLoading(true);
+    try {
+      const model = await mobilenet.load();
+      setModel(model);
+      setIsModelLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsModelLoading(false);
+    }
+  }
+
+  const uploadImage = (e) => {
+    const { files } = e.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(files[0]);
+      setImageUrl(url);
+    } else {
+      setImageUrl(null);
+    }
+  }
+
+  useEffect(() => {
+    loadModel()
+  }, []);
+
+  if (isModelLoading) {
+    return <h2>Model Loading...</h2>
+  }
+
   return (
     <div className="App">
-      <h1>Image Identification</h1>
+      <h1 className="header">Image Identification</ h1>
+      <div className="inputHolder">
+        <input type="file" accept="image/*" capture="camera" className="uploadInput" onChange={uploadImage}></input>
+      </div>
     </div>
   );
 }
